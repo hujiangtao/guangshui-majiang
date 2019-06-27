abe4dda30d214fc29efd8e202ac6d723cea1b0f6
Jonathan Whoo <jwhoo@localhost.localdomain>
2019-06-28 01:00:41 +0800

    更改ip

--- web-server/app.js
+++ web-server/app.js
@@ -22,5 +22,5 @@ app.configure('production', function(){
 	app.use(express.errorHandler());
 });
 
-console.log("Web server has started.\nPlease log on http://127.0.0.1:3001/index.html");
+console.log("Web server has started.\nPlease log on http://192.168.16.118:3001/index.html");
 app.listen(3001);
